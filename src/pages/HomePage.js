64d9b8bc0bc91fd1fac61087926e3948d0f8d860
jack-skelton <jack.s1995@live.com.au>
2017-06-14 00:09:27 +1000

    added sass

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,11 +5,11 @@ export default class Home extends Component {
 render() {
   return (
     <div id="home">
-        <div id="home-title">
-          <h1>Josh Chase</h1>
-          <h3>Perth's elite female companion</h3>
-        </div>
-      </div>
+      <section id="home-title">
+        <h1>Josh Chase</h1>
+        <h3>Perth's elite female companion</h3>
+      </section>
+    </div>
     );
   }
 }
