f85fa7173976186496ef99ea0bcc5d4f18d2df78
karlcode <karl.l1@hotmail.com>
2017-06-13 15:13:59 +1000

    Front page render

--- src/App.js
+++ src/App.js
@@ -14,27 +14,26 @@ import Contact from './components/Contact'
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router >
         <div >
+          <nav>
           <ul className="navigation">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/services">Services and Rates</Link></li>
             <li><Link to="/faq">FAQ</Link></li>
             <li><Link to="/contact">Contact</Link></li>
+            
           </ul>
-
-          <div className="container">
-            <div className="half">
+          </nav>
+          
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/services" component={Services}/>
             <Route path="/faq" component={FAQ}/>
             <Route path="/contact" component={Contact}/>
-            </div>
-           
-          </div>
-        </div>
+            
+       </div>
       </Router>
     );
   }
