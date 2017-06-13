7bd095e504dac02e08dc4694a27c0030c2adff24
jack-skelton <jack.s1995@live.com.au>
2017-06-13 20:21:11 +1000

    file restructuring

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Link
 } from 'react-router-dom'
 import { CSSTransitionGroup } from 'react-transition-group'
-import Home from './components/Home'
+import Home from './pages/HomePage'
 import About from './components/About'
 import Services from './components/Services'
 import FAQ from './components/FAQ'
@@ -33,13 +33,11 @@ class App extends Component {
             transitionAppearTimeout={500}
             transitionEnter={false}
             transitionLeave={false}>
-            <div className="half">
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/services" component={Services}/>
             <Route path="/faq" component={FAQ}/>
             <Route path="/contact" component={Contact}/>
-            </div>
             </CSSTransitionGroup>
           </div>
         </div>
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./HomePage.css";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="landing-bg"/>
+                <h1>gregre</h1>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
